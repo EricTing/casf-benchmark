c4247f4dbb263e22d076e16c9aa8ddf5661408ce
jaydy <jaydy@qb.loni.org>
2016-04-11 14:22:46 -0500

    generate pdbqt for ligands
    	modified:   casf_benchmark/paths.py
    	new file:   casf_benchmark/vina.py
    	new file:   dat/casf_names.txt

--- casf_benchmark/paths.py
+++ casf_benchmark/paths.py
@@ -2,6 +2,7 @@ from os import path, makedirs
 
 DAT = "/work/jaydy/dat/website-core-set/"
 DOMAIN_DAT = "/work/jaydy/dat/dom"
+VINA_INPUT = "/work/jaydy/dat/website-core-set/input"
 
 
 class Path:
@@ -24,3 +25,12 @@ class Domain:
     def __init__(self, id):
         self.id = id
         self.dat_dir = path.join(DOMAIN_DAT, self.id)
+
+
+class VinaPath:
+    def __init__(self, id):
+        "Paths for the inputs and outputs benchmarking CASF using Vina"
+        self.id = id
+        self.lig_pdbqt = path.join(VINA_INPUT, 'ligand-pdbqt', self.id[:4] + "_ligand.pdbqt")
+        self.lig_sdf = path.join(VINA_INPUT, 'ligand-sdf',
+                                 self.id[:4] + "_ligand.sdf")
