f574542f696d4f045f5297d13a3111561a3c3d34
jaydy <jaydy@qb.loni.org>
2016-04-14 22:13:56 -0500

    benchmark GeauxDock using predicted binding pocket
    1. calculate trace
    
    	deleted:    README.md
    	new file:   README.org
    	modified:   casf_benchmark/casf_geauxdock.py
    	new file:   casf_benchmark/trace.bash
    	new file:   dat/geaux_pred_rmsd.json

--- casf_benchmark/casf_geauxdock.py
+++ casf_benchmark/casf_geauxdock.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
 from paths import VinaPath, GEAUX_OUTPUT
+from dockedpose import rmsd_between  # https://gist.github.com/EricTing/4a540c8e13321954d2f3
 import pandas as pd
+import json
+import pybel
 import luigi
 import os
 import add_lig_atom_types
@@ -39,6 +42,31 @@ def trace():
         foo(tname)
 
 
+def eval_rmsd():
+    """evaluate the geauxdock prediction using RMSD
+    """
+
+    def foo(tname):
+        try:
+            geaux_sdf = os.path.join(GEAUX_OUTPUT, tname, tname + '_new_0.sdf')
+            native_sdf = VinaPath(tname).lig_sdf
+
+            geaux_lig = pybel.readfile("sdf", geaux_sdf).next()
+            native_lig = pybel.readfile("sdf", native_sdf).next()
+            result_rmsd = rmsd_between(native_lig, geaux_lig)
+            return result_rmsd
+        except Exception as e:
+            print(e)
+            return None
+
+    data = {}
+    for tname in [_.rstrip() for _ in file("../dat/casf_names.txt")]:
+        data[tname] = foo(tname)
+
+    with open("../dat/geaux_pred_rmsd.json", 'w') as ofs:
+        ofs.write(json.dumps(data, indent=4, separators=(',', ': ')))
+
+
 def main():
     for tname in [_.rstrip() for _ in file("../dat/casf_names.txt")]:
         luigi.build([AddTypes(tname)], local_scheduler=True)
@@ -47,4 +75,5 @@ def main():
 if __name__ == '__main__':
     # main()
     # test()
-    trace()
+    # trace()
+    eval_rmsd()
---
+++ casf_benchmark/trace.bash
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+readonly bin=~/local/bin/dock
+# readonly bin=~/Workspace/Bitbucket/geauxdock/src/dock
+
+readonly PDB_DIR=/work/jaydy/dat/website-core-set/input/protein-pdb
+
+readonly SDF_DIR=/work/jaydy/dat/website-core-set/input/ligand-sdf
+
+readonly FF_DIR=/work/jaydy/dat/website-core-set/input/params_ff
+
+readonly paras=/home/jaydy/Workspace/Bitbucket/geauxdock/data/parameters/paras
+
+readonly OUT_DIR=/work/jaydy/dat/website-core-set/output
+
+trace() {
+    complex=$1
+
+    pdb_file=$PDB_DIR/${complex}.pdb
+    sdf_file=$SDF_DIR/$(echo ${complex:0:4})_ligand.sdf
+    ff_file=$FF_DIR/${complex}.ff
+
+    out_dir=$OUT_DIR/${complex}
+    mkdir -p $out_dir
+
+    csv_file=$out_dir/${complex}_trace.csv
+    conf_file=$out_dir/${complex}_new
+
+
+    cmd="\
+${bin} \
+--id ${complex} \
+-p ${pdb_file} \
+-l ${sdf_file} \
+-s ${ff_file} \
+\
+--para ${paras} \
+--trace \
+--conf $conf_file \
+\
+--csv $csv_file \
+ --nc 10 \
+--floor_temp 0.01 \
+--ceiling_temp 0.036 \
+--nt 1 \
+-t 0.02 \
+-r 0.08 \
+"
+
+
+    echo ${cmd}
+    ${cmd}
+}
+
+for complex in `cat ../dat/casf_names.txt `; do
+    trace $complex
+done
